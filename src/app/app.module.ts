b7be6a52ba9425c5b64ace11501ab5d647d7c46b
tanjeeb211@live.com <tanjeeb211@live.com>
2019-02-28 21:16:10 +0600

    add some design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,33 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
+export const firebaseConfig={
+  apiKey: "AIzaSyCVAJOfZDaTSewkZVp7Ozja6fFgYXBESus",
+  authDomain: "ngtest-d2cb9.firebaseapp.com",
+  databaseURL: "https://ngtest-d2cb9.firebaseio.com",
+  projectId: "ngtest-d2cb9",
+  storageBucket: "ngtest-d2cb9.appspot.com",
+  messagingSenderId: "1050350470327"
+};
+
+
 
 @NgModule({
   declarations: [
     AppComponent
+
   ],
   imports: [
     BrowserModule,
+      FormsModule,
+      HttpModule,
+      AngularFireModule.initializeApp(firebaseConfig),
+      AngularFireDatabaseModule,
     AppRoutingModule
   ],
   providers: [],
